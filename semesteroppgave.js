72eecc1f2ed72c3b7b882dbad754fce7dcc351d0
Anne-Lise Ackenhausen <anne-lise.ackenhausen@student.uib.no>
2019-04-12 15:38:24 +0200

    endret på konstruktør på js

--- semesteroppgave.js
+++ semesteroppgave.js
@@ -1,6 +1,8 @@
 //Kostruktør for alle
-function URL(url) {
-  this.url = url;
+function URL(urlq, url2, url3, getnames, getIDs, getInfo, load) {
+  this.url1 = url1;
+  this.url2 = url2;
+  this.url3 = url3;
 }
 
 //Returnerer listen av alle kommunanavnene
@@ -33,7 +35,16 @@ function getIDs(url) {
 
 //Bruker kommunenummeret som argument og henter all informasjon om den kommunen
 function  getInfo(url, n) {
-
+  var xhr = new XMLHttpRequest;
+  xhr.open( "GET", url);
+  xhr.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200){
+      console.log("Type", xhr.getResponseHeader("Content-Type"));
+      var kommune = JSON.parse(xhr.responseText)
+      console.log(kommune.region.verdi);
+    }
+  };
+  xhr.send();
 }
 
 //
