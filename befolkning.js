aba2074f7ef58ff7f988a0cddd60c1b433777fc9
bendikbp <48202511+bendikbp@users.noreply.github.com>
2019-04-26 15:13:15 +0200

    Update befolkning.js

--- befolkning.js
+++ befolkning.js
@@ -2,14 +2,14 @@ function Urll(url) {
     this.url = "http://wildboy.uib.no/~tpe056/folk/" + url + ".json";
     
     this.oversikt = function() {
-        document.getElementById("overview").innerHTML = "<th>Kommune</th><th>Kommunenummer</th><th>År</th><th>Menn</th><th>Kvinner</th>";
+        document.getElementById("overview").innerHTML = "<tr><th colspan='3'></th><th colspan='3'>Befolkning</th></tr><th>Kommune</th><th>Kommunenummer</th><th>År</th><th>Menn</th><th>Kvinner</th>";
         http(function(data) {
-            for (x in data.elementer) {
-                var år = Object.keys(data.elementer[x].Kvinner);
-                var kommune = data.elementer[x].kommunenummer;
-                var menn = data.elementer[x].Menn;
-                var kvinner = data.elementer[x].Kvinner;
-
+            for (kommune in data.elementer) {
+                var år = Object.keys(data.elementer[kommune].Kvinner);
+                var kommunenr = data.elementer[kommune].kommunenummer;
+                var menn = data.elementer[kommune].Menn;
+                var kvinner = data.elementer[kommune].Kvinner;
+                
                 for (y in menn) {
                     var mann = menn[y];
                 }
@@ -19,23 +19,24 @@ function Urll(url) {
                 for (tall in år) {
                     var year = år[tall];
                 }
-                document.getElementById("overview").innerHTML += "<tr><td>" + x + "</td><td>" + kommune + "</td><td>" + year + "</td>" + "<td>" + mann + "</td><td>" + kvinne + "</td></tr>";
-                }  
+                document.getElementById("overview").innerHTML += "<tr><td>" + kommune + "</td><td>" + kommunenr + "</td><td>" + year + "</td>" + "<td>" + mann + "</td><td>" + kvinne + "</td></tr>";
+                }
         });
     };
     
     this.getAll = function() {
-        document.getElementById("detail").innerHTML = "<tr><th colspan='5'></th><th colspan='3'>Menn</th><th colspan='3'>Kvinner</th></tr><th>Kommune</th><th>Kommunenummer</th><th>År</th><th>Menn</th><th>Kvinner</th><th>Fagskole</th><th>Uni kort</th><th>Uni lang</th><th>Fagskole</th><th>Uni kort</th><th>Uni lang</th>";
+        console.log("getall befolkning.js");
+        document.getElementById("detail").innerHTML = "<tr><th colspan='5'></th><th colspan='3'>Menn</th><th colspan='3'>Kvinner</th></tr><th>Kommune</th><th>Kommunenummer</th><th>År</th><th>Menn</th><th>Kvinner</th><th>Grunnskole</th><th>Fagskole</th><th>Uni kort</th><th>Uni lang</th><th>Grunnskole</th><th>Fagskole</th><th>Uni kort</th><th>Uni lang</th>";
         
         var syssel = syss.getAll();
         var danning = utdanning.getAll();
         
         http(function(data) {
-            for (x in data.elementer) {
-                var år = Object.keys(data.elementer[x].Kvinner);
-                var kommune = data.elementer[x].kommunenummer;
-                var menn = data.elementer[x].Menn;
-                var kvinner = data.elementer[x].Kvinner;
+            for (kommune in data.elementer) {
+                var år = Object.keys(data.elementer[kommune].Kvinner);
+                var kommunenr = data.elementer[kommune].kommunenummer;
+                var menn = data.elementer[kommune].Menn;
+                var kvinner = data.elementer[kommune].Kvinner;
 
                 for (y in menn) {
                     var mann = menn[y];
@@ -51,60 +52,61 @@ function Urll(url) {
                     var testing2 = listeSyssel[1];
                 }
                 for (var row in listeUtdanning) {
-                    var fagM = listeUtdanning[0];
-                    var fagK = listeUtdanning[1];
-                    var uniMK = listeUtdanning[2];
-                    var uniKK = listeUtdanning[3];
-                    var uniML = listeUtdanning[4];
-                    var uniKL = listeUtdanning[5];
+                    var grunnM = listeUtdanning[0];
+                    var grunnK = listeUtdanning[1];
+                    var fagM = listeUtdanning[2];
+                    var fagK = listeUtdanning[3];
+                    var uniMK = listeUtdanning[4];
+                    var uniKK = listeUtdanning[5];
+                    var uniML = listeUtdanning[6];
+                    var uniKL = listeUtdanning[7];
                 }
-                document.getElementById("detail").innerHTML += "<tr><td>" + x + "</td><td>" + kommune + "</td><td>" + year + "</td><td>" + mann + "</td><td>" + kvinne + "</td><td>" + fagM + "</td><td>" + fagK + "</td><td>" + uniMK + "</td><td>" + uniKK + "</td><td>" + uniML + "</td><td>" + uniKL + "</td></tr>";
+                document.getElementById("detail").innerHTML += "<tr><td>" + kommune + "</td><td>" + kommunenr + "</td><td>" + year + "</td><td>" + grunnM + "</td><td>" + grunnK + "</td><td>" + mann + "</td><td>" + kvinne + "</td><td>" + fagM + "</td><td>" + fagK + "</td><td>" + uniMK + "</td><td>" + uniKK + "</td><td>" + uniML + "</td><td>" + uniKL + "</td></tr>";
             }
         });
     };
     
     //Laster inn info om en kommune ved hjelp av kommunenummeret
     this.getInfo = function() {        
-        document.getElementById("detail").innerHTML = "<tr><th colspan='5'></th><th colspan='3'>Menn</th><th colspan='3'>Kvinner</th></tr><th>Kommune</th><th>Kommunenummer</th><th>År</th><th>Menn</th><th>Kvinner</th><th>Fagskole</th><th>Uni kort</th><th>Uni lang</th><th>Fagskole</th><th>Uni kort</th><th>Uni lang</th>";
+        document.getElementById("detail").innerHTML = "<tr><th colspan='5'></th><th colspan='6'>Menn</th><th colspan='6'>Kvinner</th></tr><th>Kommune</th><th>Kommunenummer</th><th>År</th><th>Menn</th><th>Kvinner</th><th>Grunnskole</th><th>VGS</th><th>Fagskole</th><th>Uni kort</th><th>Uni lang</th><th>Uoppgitt</th><th>Grunnskole</th><th>VGS</th><th>Fagskole</th><th>Uni kort</th><th>Uni lang</th><th>Uoppgitt</th>";
         
         var syssel = syss.getInfo();
         var danning = utdanning.getInfo();
         
         http(function(data) {
             var kNummer = document.getElementById("kommunenummer").value;
-            for (x in data.elementer) {
-                if (data.elementer[x].kommunenummer == kNummer) {
-                    var år = Object.keys(data.elementer[x].Kvinner);
-                    var kommune = data.elementer[x].kommunenummer;
-                    var menn = data.elementer[x].Menn;
-                    var kvinner = data.elementer[x].Kvinner;
+            for (kommune in data.elementer) {
+                if (data.elementer[kommune].kommunenummer == kNummer) {
+                    var kommunenr = data.elementer[kommune].kommunenummer;
+                    var menn = data.elementer[kommune].Menn;
+                    var kvinner = data.elementer[kommune].Kvinner;
 
                     for (y in menn) {
                         var mann = menn[y];
                     }
-                    
                     for (z in kvinner) {
                         var kvinne = kvinner[z];
                     } 
-                    
-                    for (tall in år) {
-                        var year = år[tall];
-                    }
-                    
                     for (var row in listeSyssel) {
                         var testing1 = listeSyssel[0];
                         var testing2 = listeSyssel[1];
                     }
-                    
                     for (var row in listeUtdanning) {
-                        var fagM = listeUtdanning[0];
-                        var fagK = listeUtdanning[1];
-                        var uniMK = listeUtdanning[2];
-                        var uniKK = listeUtdanning[3];
-                        var uniML = listeUtdanning[4];
-                        var uniKL = listeUtdanning[5];
+                    var grunnM = listeUtdanning[0];
+                    var grunnK = listeUtdanning[1];
+                    var vgsM = listeUtdanning[2];
+                    var vgsK = listeUtdanning[3];
+                    var fagM = listeUtdanning[4];
+                    var fagK = listeUtdanning[5];
+                    var uniMK = listeUtdanning[6];
+                    var uniKK = listeUtdanning[7];
+                    var uniML = listeUtdanning[8];
+                    var uniKL = listeUtdanning[9];
+                    var uoppM = listeUtdanning[10];
+                    var uoppK = listeUtdanning[11];
+                    var year = listeUtdanning[12];
                     }
-                    document.getElementById("detail").innerHTML += "<tr><td>" + x + "</td><td>" + kommune + "</td><td>" + year + "</td><td>" + mann + "</td><td>" + kvinne + "</td><td>" + fagM + "</td><td>" + fagK + "</td><td>" + uniMK + "</td><td>" + uniKK + "</td><td>" + uniML + "</td><td>" + uniKL + "</td></tr>";
+                    document.getElementById("detail").innerHTML += "<tr><td>" + kommune + "</td><td>" + kommunenr + "</td><td>" + year + "</td><td>" + mann + "</td><td>" + kvinne + "</td><td>" + grunnM + "%</td><td>" + vgsM + "%</td><td>" + fagM + "%</td><td>" + uniMK + "%</td><td>" + uniML + "</td><td>" + uoppM + "%</td><td>" + grunnK + "%</td><td>" + vgsK + "%</td><td>" + fagK + "%</td><td>" + uniKK + "%</td><td>" + uniKL + "%</td><td>" + uoppK + "%</td></tr>";
                 }
             }   
         });
@@ -123,35 +125,33 @@ function Urll(url) {
             var female1 = [];
             var kommuneEn;
             
-            for (x in data.elementer) {
-                if (data.elementer[x].kommunenummer == kNummer1) {
-                    kommuneEn = x;
-                    var år = Object.keys(data.elementer[x].Kvinner);
-                    var kommune = data.elementer[x].kommunenummer;
-                    var menn = data.elementer[x].Menn;
-                    var kvinner = data.elementer[x].Kvinner;
+            for (kommune in data.elementer) {
+                if (data.elementer[kommune].kommunenummer == kNummer1) {
+                    kommuneEn = kommune;
+                    var år = Object.keys(data.elementer[kommune].Kvinner);
+                    var kommunenr = data.elementer[kommune].kommunenummer;
+                    var menn = data.elementer[kommune].Menn;
+                    var kvinner = data.elementer[kommune].Kvinner;
                     
                     for (i in kMennTo) {
                         male1.push(kMennTo[i]);
                     }
-                    
                     for (i in kKvinnerTo) {
                         female1.push(kKvinnerTo[i])
                     }
                 }
             }
             
-            for (x in data.elementer) {
-                if (data.elementer[x].kommunenummer == kNummer2) {
+            for (kommune in data.elementer) {
+                if (data.elementer[kommune].kommunenummer == kNummer2) {
                     var text = document.getElementById("sammenligning");
-                    var table = "<table><thead><tr><th></th><th colspan='3'>Menn</th><th colspan='3'>Kvinner</th></tr><th>År</th><th>" + kommuneEn + "</th><th></th><th>" + x + "</th><th>" + kommuneEn + "</th><th></th><th>" + x + "</th></thead><tbody>";
+                    var table = "<table><thead><tr><th></th><th colspan='3'>Menn</th><th colspan='3'>Kvinner</th></tr><th>År</th><th>" + kommuneEn + "</th><th></th><th>" + kommune + "</th><th>" + kommuneEn + "</th><th></th><th>" + kommune + "</th></thead><tbody>";
                                 
-                    var år = Object.keys(data.elementer[x].Kvinner);
-                    var kommune = data.elementer[x].kommunenummer;
-                    var menn = data.elementer[x].Menn;
-                    var kvinner = data.elementer[x].Kvinner;
+                    var år = Object.keys(data.elementer[kommune].Kvinner);
+                    var kommunenr = data.elementer[kommune].kommunenummer;
+                    var menn = data.elementer[kommune].Menn;
+                    var kvinner = data.elementer[kommune].Kvinner;
                     var biggest; 
-                    
                     var years = [];
                     var male2 = [];
                     var female2 = [];
@@ -159,29 +159,25 @@ function Urll(url) {
                     for (var  i=0; i<år.length; i++) {
                         years.push(år[i])
                     }
-                    
                     for (i in kMennEn) {
                         male2.push(kMennEn[i]);
                     }
-                    
                     for (i in kKvinnerEn) {
                         female2.push(kKvinnerEn[i])
                     }
-                
-                    for(var i=0; i<år.length; i++) {
-                        if(male2[i] > male1[i]) {
+                    for (var i=0; i<år.length; i++) {
+                        if (male2[i] > male1[i]) {
                             mbiggest = "&gt;";
-                        }else{
+                        } else {
                             mbiggest = "&lt;";
                         }
-                        
-                        if(female2[i] > female1[i]) {
+                        if (female2[i] > female1[i]) {
                             fbiggest = "&gt;";
-                        }else{
+                        } else {
                             fbiggest = "&lt;";
                         }
                         
-                        table += "<tr><td>" + years[i] + "</td><td>" + male2[i] + "%" + "</td><td>" + "<p id='greaterOrEqual'>" + mbiggest + "</p></td><td>" + male1[i] + "%" + "</td><td>" + female2[i] + "%" + "</td><td>" + "<p id='greaterOrEqual'>" + fbiggest + "</p>" + "</td><td>" + female1[i] + "%" + "</td></tr>";
+                        table += "<tr><td>" + years[i] + "</td><td>" + male2[i] + "%</td><td>" + "<p id='greaterOrEqual'>" + mbiggest + "</p></td><td>" + male1[i] + "%</td><td>" + female2[i] + "%</td><td>" + "<p id='greaterOrEqual'>" + fbiggest + "</p>" + "</td><td>" + female1[i] + "%</td></tr>";
                     }
                     table += "</tbody></table>";
                     text.innerHTML = table;
@@ -223,6 +219,6 @@ function http(cb) {
             cb(networkObj);
         }
     }
-    xhr.open("GET", url.url, true);
+    xhr.open("GET", url.url, false);
     xhr.send();
 };
